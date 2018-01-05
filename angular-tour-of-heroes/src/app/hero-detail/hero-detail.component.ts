1a4854fcdb45c24e523e4d8439882f2db757df1e
Mylon <mylonsuren@yahoo.ca>
2018-01-05 15:43:53 -0500

    updated routing, hero-detail components, and dashboard components

--- angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
+++ angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
 
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { HeroService }  from '../hero.service';
+
 import { Hero } from '../hero';
 
 @Component({
@@ -11,9 +16,20 @@ export class HeroDetailComponent implements OnInit {
 
     @Input() hero: Hero;
 
-    constructor() { }
+    constructor(private route: ActivatedRoute, private heroService: HeroService, private location: Location) { }
 
-    ngOnInit() {
+    ngOnInit(): void {
+        this.getHero();
+    }
+
+    getHero(): void {
+        const id = +this.route.snapshot.paramMap.get('id');
+        this.heroService.getHero(id)
+            .subscribe(hero => this.hero = hero);
+    }
+
+    goBack(): void {
+      this.location.back();
     }
 
 }
--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -8,31 +8,19 @@ import { HeroService } from '../hero.service';
     templateUrl: './heroes.component.html',
     styleUrls: ['./heroes.component.css']
 })
+
+
 export class HeroesComponent implements OnInit {
+  heroes: Hero[];
 
-    heroes: Hero[];
+  constructor(private heroService: HeroService) { }
 
-    hero: Hero = {
-        id: 1,
-        name: 'Windstorm'
-    };
+  ngOnInit() {
+    this.getHeroes();
+  }
 
-
-    constructor(private heroService: HeroService) { }
-
-    ngOnInit() {
-        this.getHeroes();
-    }
-
-    selectedHero: Hero;
-    onSelect(hero: Hero): void {
-        this.selectedHero = hero;
-    }
-
-
-    getHeroes(): void {
-        this.heroService.getHeroes()
-            .subscribe(heroes => this.heroes = heroes);
-    }
-
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
+  }
 }
