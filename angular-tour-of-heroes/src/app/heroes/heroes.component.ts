45eabceb8c9a3f54cdf50352fa9e21b6553490f9
Mylon <mylonsuren@yahoo.ca>
2018-01-05 15:04:39 -0500

    added more components and services

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
     selector: 'app-heroes',
@@ -10,7 +10,7 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-    heroes = HEROES;
+    heroes: Hero[];
 
     hero: Hero = {
         id: 1,
@@ -18,14 +18,21 @@ export class HeroesComponent implements OnInit {
     };
 
 
-    constructor() { }
+    constructor(private heroService: HeroService) { }
 
-    ngOnInit() {}
+    ngOnInit() {
+        this.getHeroes();
+    }
 
     selectedHero: Hero;
-
     onSelect(hero: Hero): void {
-      this.selectedHero = hero;
+        this.selectedHero = hero;
+    }
+
+
+    getHeroes(): void {
+        this.heroService.getHeroes()
+            .subscribe(heroes => this.heroes = heroes);
     }
 
 }
